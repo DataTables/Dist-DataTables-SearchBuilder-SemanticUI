f0dc71b49623ddfe67827836a179cc8bb6c44c11
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:12 +0000

    Add support for mjs files (ES Modules)

---
+++ js/searchBuilder.semanticui.mjs
@@ -0,0 +1,41 @@
+
+/*! semantic ui integration for DataTables' SearchBuilder
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-se';
+import DataTable from 'datatables.net-searchbuilder';
+
+
+$.extend(true, DataTable.SearchBuilder.classes, {
+    clearAll: 'basic ui button dtsb-clearAll'
+});
+$.extend(true, DataTable.Group.classes, {
+    add: 'basic ui button dtsb-add',
+    clearGroup: 'basic ui button dtsb-clearGroup',
+    logic: 'basic ui button dtsb-logic'
+});
+$.extend(true, DataTable.Criteria.classes, {
+    condition: 'ui selection dropdown dtsb-condition',
+    data: 'ui selection dropdown dtsb-data',
+    "delete": 'basic ui button dtsb-delete',
+    left: 'basic ui button dtsb-left',
+    right: 'basic ui button dtsb-right',
+    value: 'basic ui selection dropdown dtsb-value'
+});
+DataTable.ext.buttons.searchBuilder.action = function (e, dt, node, config) {
+    e.stopPropagation();
+    this.popover(config._searchBuilder.getNode(), {
+        align: 'container',
+        span: 'container'
+    });
+    // Need to redraw the contents to calculate the correct positions for the elements
+    if (config._searchBuilder.s.topGroup !== undefined) {
+        config._searchBuilder.s.topGroup.dom.container.trigger('dtsb-redrawContents');
+    }
+    $('div.dtsb-searchBuilder').removeClass('ui basic vertical buttons');
+};
+
+
+export default DataTable;
