0f1d83ad8d9e367f7212bf4b5bd1b21ef4d91424
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-10-10 10:01:37 +0100

    b29d74a762c29d82051e42eb4eac67ebb1794871 Fix: Styling integration for the new "Search" button feature - classes were missing
    https://datatables.net/forums/discussion/77320
    
    Sync to source repo @b29d74a762c29d82051e42eb4eac67ebb1794871

--- js/searchBuilder.semanticui.mjs
+++ js/searchBuilder.semanticui.mjs
@@ -23,6 +23,7 @@ $.extend(true, DataTable.Criteria.classes, {
     "delete": 'basic ui button dtsb-delete',
     left: 'basic ui button dtsb-left',
     right: 'basic ui button dtsb-right',
+    search: 'basic ui button dtsb-search',
     value: 'basic ui selection dropdown dtsb-value'
 });
 DataTable.ext.buttons.searchBuilder.action = function (e, dt, node, config) {
