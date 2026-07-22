3d1e8033e12301448cadcb993417c8f3c744f45e
dtbuild <dtbuild@rigil>
2026-07-22 14:50:44 +0000

    b2567bef1a733d021110553c86991410013af8de Release 2.0.0 ready
    Sync to source repo @b2567bef1a733d021110553c86991410013af8de

--- js/searchBuilder.semanticui.mjs
+++ js/searchBuilder.semanticui.mjs
@@ -1,4 +1,4 @@
-/*! SearchBuilder Fomantic styling 2.0.0-beta.1 for DataTables
+/*! SearchBuilder Fomantic styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
